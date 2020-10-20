1f40ade809b59e6c011be508f910b4d86493256b
jannamryan1997 <jannamryan1997>
2020-10-19 19:31:46 -0700

    created design

--- src/app/views/main/home/home.module.ts
+++ src/app/views/main/home/home.module.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeViewComponent } from './page/home.view';
 
 @NgModule({
     declarations: [HomeViewComponent],
-    imports: [HomeRoutingModule],
+    imports: [HomeRoutingModule, CommonModule, FormsModule, ReactiveFormsModule],
     exports: []
 })
 export class HomeModule { }
--- src/app/views/main/home/page/home.view.ts
+++ src/app/views/main/home/page/home.view.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
+declare const google;
+
 @Component({
     selector: 'app-home-view',
     templateUrl: 'home.view.html',
@@ -10,7 +12,15 @@ export class HomeViewComponent implements OnInit, OnDestroy {
 
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this._initMap();
+    }
 
+     private _initMap(): void {
+       const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+          center: { lat: 40.828230, lng: 43.823510 },
+          zoom: 8,
+        });
+      }
     ngOnDestroy() { }
 }
